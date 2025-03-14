77042cc9a80d76da0b90b09efde842995460b68c
George Udonte <georgeudonte@gmail.com>
2025-03-14 15:24:04 +0100

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-red-500 text-white">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
